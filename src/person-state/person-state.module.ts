7347c6b91e049135c70f0af293240f3f0f294312
darius1751 <lupequi12@gmail.com>
2023-02-12 16:00:41 -0500

    importing TypeOrmModule in module person-state

--- src/person-state/person-state.module.ts
+++ src/person-state/person-state.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PersonStateService } from './person-state.service';
 import { PersonStateController } from './person-state.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonState } from './entities/person-state.entity';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([PersonState])
+  ],
   controllers: [PersonStateController],
   providers: [PersonStateService]
 })
